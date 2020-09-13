d60957f10af296a1e6e8cbfc569aa3774b7f7e32
erfantkerfan <erfantkerfan@yahoo.com>
2020-09-13 20:39:33 +0430

    implemented autoupdate

---
+++ install-app.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+color 3f
+ECHO.
+ECHO.
+set /p branch=Enter Branch name:
+ECHO ============== CLONING ==============
+git clone https://github.com/alaatv/studio-app -b %branch%
+ECHO.
+ECHO.
+ECHO ============== INITIALIZING ==============
+pip install -r requirements.txt
+ECHO.
+ECHO.
+ECHO ================== DONE ==================
+pause
