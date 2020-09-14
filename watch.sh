4c736f6597071d49d4ecec7d27e86e430bd4f972
erfantkerfan <erfantkerfan@yahoo.com>
2020-09-14 12:25:13 +0430

    improve watch shell

--- watch.sh
+++ watch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 shopt -s extglob
-array=( axis convert upload )
-for log in "${array[@]}"
-do
-        journalctl -f --no-pager -n 100 --output=cat -u studio-$log &
-done
-
+if [ $# -eq 0 ]
+then
+        array=( axis convert upload )
+        for log in "${array[@]}"
+        do
+                journalctl -f --no-pager -n 100 --output=cat -u studio-$log &
+        done
+else
+        journalctl -f --no-pager -n 100 --output=cat -u studio-$1
+fi
 wait
