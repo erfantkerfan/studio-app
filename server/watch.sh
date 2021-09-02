03b75b347f2c245d64eb481843eeb8005de151d6
erfantkerfan <erfantkerfan@yahoo.com>
2021-09-02 21:17:46 +0430

    reformat watcher

--- server/watch.sh
+++ server/watch.sh
@@ -1,14 +1,12 @@
 #/bin/bash
 #shopt -s extglob
 trap 'kill $(jobs -p)' EXIT
-if [ $# -eq 0 ]
-then
-        array=( axis convert upload )
-        for log in "${array[@]}"
-	do
-                journalctl -f --no-pager -n 1000 --output=cat -u studio-$log &
-        done
+if [ $# -eq 0 ]; then
+  array=(axis convert upload)
+  for log in "${array[@]}"; do
+    journalctl -f --no-pager -n 1000 --output=cat -u studio-$log &
+  done
 else
-        journalctl -f --no-pager -n 1000 --output=cat -u studio-$1
+  journalctl -f --no-pager -n 1000 --output=cat -u studio-$1
 fi
 wait
